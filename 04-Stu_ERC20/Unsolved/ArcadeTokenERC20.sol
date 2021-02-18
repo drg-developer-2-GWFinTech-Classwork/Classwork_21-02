5b0575aec17537d2e5a64319b130df86f6bc5c2a
David G <davidrgayman@gmail.com>
2021-02-17 23:52:14 -0500

    Working on the class examples.

--- 04-Stu_ERC20/Unsolved/ArcadeTokenERC20.sol
+++ 04-Stu_ERC20/Unsolved/ArcadeTokenERC20.sol
@@ -8,18 +8,22 @@ contract ArcadeToken is ERC20, ERC20Detailed {
 
     modifier onlyOwner {
         // @TODO: add a `require` to check if `owner` is the `msg.sender`
+        require(msg.sender == owner, "The owner is not the sender.");
         _; // this underscore sends us back to the function that called this modifier
     }
 
     // @TODO: Pass the required parameters to `ERC20Detailed`
-    constructor(uint initial_supply) ERC20Detailed() public {
+    constructor(uint initial_supply) ERC20Detailed("ArcadeToken", "ARCD", 16) public {
         // @TODO: Set the owner to be `msg.sender`
+        owner = msg.sender;
 
         // @TODO: Call the internal `_mint` function to give `initial_supply` to the `owner`
-    }
+        _mint(owner, initial_supply);
+    } 
 
     // @TODO: Add the `onlyOwner` modifier to this function after `public`
-    function mint(address recipient, uint amount) public {
+    function mint(address recipient, uint amount) public onlyOwner {
         // @TODO: Call the internal `_mint` function and pass the `recipient` and `amount` variables
+        _mint(recipient, amount);
     }
 }
